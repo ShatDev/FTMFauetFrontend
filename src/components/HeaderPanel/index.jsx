8238976aba575e8d7a2b9533e7d9059b1dab4e19
jason <kwon_jason@yahoo.com>
2021-01-27 19:28:41 +0300

    fix

--- src/components/HeaderPanel/index.jsx
+++ src/components/HeaderPanel/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState ,useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import TextField from '@material-ui/core/TextField';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -9,48 +9,50 @@ import FormHelperText from '@material-ui/core/FormHelperText';
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
 import coinAddressValidator from 'coin-address-validator';
-import './styles.css'
+import './styles.css';
 
-const HeaderPanel = (props) => {
-
+const HeaderPanel = props => {
   useEffect(() => {
-    fetch('http://18.207.251.49:4006/status/ftm').then(res => res.json()).then(result => {
-      setAddress(result.address)
-      setFtm(result.balance)
-    })
-  },[])
+    fetch('http://18.207.251.49:4006/status/ftm')
+      .then(res => res.json())
+      .then(result => {
+        setAddress(result.address);
+        setFtm(result.balance);
+      });
+  }, []);
 
-  const [address, setAddress] = useState()
-  const [ftm,setFtm] = useState()
+  const [address, setAddress] = useState();
+  const [ftm, setFtm] = useState();
 
-  const [displayMsg,setDisplayMsg] = useState('')
+  const [displayMsg, setDisplayMsg] = useState('');
 
-  const [targetAddress,setTargetAddress] = useState('')
+  const [targetAddress, setTargetAddress] = useState('');
 
-  const handleAddressChange = (e) => {
-    setTargetAddress(e.target.value)
-  }
+  const handleAddressChange = e => {
+    setTargetAddress(e.target.value);
+  };
 
   const validateAddress = () => {
-    return coinAddressValidator.validate(targetAddress,'eth','prod')
-    
-  }
+    return coinAddressValidator.validate(targetAddress, 'eth', 'prod');
+  };
 
   const handleGetFTM = () => {
-    let isValidAddress = validateAddress()
-    if(!isValidAddress){
-      setDisplayMsg("The address is not a valid address!")
+    let isValidAddress = validateAddress();
+    if (!isValidAddress) {
+      setDisplayMsg('The address is not a valid address!');
+      return;
     }
-    fetch('http://18.207.251.49:4006/request/ftm/' + targetAddress).then(res => res.json()).then(result =>{
-      if (result.hasOwnProperty('error')){
-        setDisplayMsg('Your account ' + result.input + ' has already ' + result.balance + 'FTM!')
-      }
-      else{
-        setDisplayMsg('10 FTMs has been sent to your address ' + targetAddress)
-        setFtm(result.remainingBalance)
-      }
-    })
-  }
+    fetch('http://18.207.251.49:4006/request/ftm/' + targetAddress)
+      .then(res => res.json())
+      .then(result => {
+        if (result.hasOwnProperty('error')) {
+          setDisplayMsg('Your account ' + result.input + ' has already ' + result.balance + 'FTM!');
+        } else {
+          setDisplayMsg('10 FTMs has been sent to your address ' + targetAddress);
+          setFtm(result.remainingBalance);
+        }
+      });
+  };
   return (
     <div className="headerContainer">
       <p>
@@ -62,34 +64,32 @@ const HeaderPanel = (props) => {
           Feel free to get FTMs to your wallet
         </font>
       </p>
-      <div className = "faucetContainer">
-        <FormControl className = 'ftmAddressInput'  variant="filled">
-            <FilledInput
-              id="filled-adornment-weight"
-              value={targetAddress}
-              placeholder = {'Input your FTM Address'}
-              onChange={handleAddressChange}
-              endAdornment={<InputAdornment position="end"></InputAdornment>}
-              aria-describedby="filled-weight-helper-text"
-              inputProps={{
-                'aria-label': 'weight',
-              }}
-            />
-            <Button
-              variant="contained"
-              color="primary"
-              className='button'
-              onClick = {handleGetFTM}
-            >
-              Get FTM
-            </Button>
-            <FormHelperText className = 'helper' id="filled-weight-helper-text">{displayMsg}</FormHelperText>
-            <FormHelperText className = 'helper' id="filled-weight-helper-text">Test Fantoms are served from <b>{address}</b> with <b>{ftm}</b> FTMs, 10 tFTMs each time.</FormHelperText>
-          </FormControl>
+      <div className="faucetContainer">
+        <FormControl className="ftmAddressInput" variant="filled">
+          <FilledInput
+            id="filled-adornment-weight"
+            value={targetAddress}
+            placeholder={'Input your FTM Address'}
+            onChange={handleAddressChange}
+            endAdornment={<InputAdornment position="end"></InputAdornment>}
+            aria-describedby="filled-weight-helper-text"
+            inputProps={{
+              'aria-label': 'weight',
+            }}
+          />
+          <Button variant="contained" color="primary" className="button" onClick={handleGetFTM}>
+            Get FTM
+          </Button>
+          <FormHelperText className="helper" id="filled-weight-helper-text">
+            {displayMsg}
+          </FormHelperText>
+          <FormHelperText className="helper" id="filled-weight-helper-text">
+            Test Fantoms are served from <b>{address}</b> with <b>{ftm}</b> FTMs, 10 tFTMs each time.
+          </FormHelperText>
+        </FormControl>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default HeaderPanel
+export default HeaderPanel;
